2a352e9eb3643a8d820365e75dfeb53ab3a01947
Aloysius Lee <16810847+arraysius@users.noreply.github.com>
2020-12-22 13:34:08 +0800

    Day 15 input and solution

---
+++ Day 15 Rambunctious Recitation/day15_part2.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ifstream file("day15_input.txt");
+
+    unordered_map<int, int> age;
+
+    int i = 1, nextNum;
+    string line;
+
+    for (i; getline(file, line, ','); i++) {
+        int n = stoi(line);
+        age.insert(make_pair(n, i));
+        nextNum = n;
+        // cout << i << ": " << n << endl;
+    }
+    age.erase(nextNum);
+    i--;
+
+    for (i; i < 30000000; i++) {
+        unordered_map<int, int>::iterator it = age.find(nextNum);
+        if (it == age.end()) {
+            age.insert(make_pair(nextNum, i));
+            nextNum = 0;
+        } else {
+            nextNum = i - it->second;
+            it->second = i;
+        }
+        // cout << i + 1 << ": " << nextNum << endl;
+    }
+
+    cout << i << ": " << nextNum << endl;
+
+    return 0;
+}
